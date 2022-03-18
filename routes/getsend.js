f229868f4e41c0ecb340397449b4bf90aeba77c6
4thdraw <4thdraw2@gmail.com>
2022-03-18 12:02:44 +0900

    노드서버연동완료

--- routes/getsend.js
+++ routes/getsend.js
@@ -1,8 +1,27 @@
 var express = require('express')
+var mysql = require('mysql')
+var dbconfig = require('./db/config.js')
+const connection = mysql.createConnection(dbconfig);
 var router = express.Router();
 
 router.get('/',(req, res) => {
     res.send('/getsend라는 요청의 응답일뿐 ');
+})
+router.get('/getjson',(req, res) => {
+    res.send({'url':'/getsend/getjson'});
+})
+router.post('/post',(req, res) => {
+    res.send({
+        'url':'/getsend/post',
+        'title':'getsend.js내의 post라우팅'
+    });
+})
+router.get('/awssql', (req, res) => {
+    connection.query('SELECT * FROM  prev_interview', (error, rows) => {
+        if (error) throw error;
+        console.log('해당스키마안의 테이블내용: ', rows);
+        res.send(rows);
+      });
 })
 
 module.exports = router;
